c6c42b0a25e6240e8e2908348b617e91dbc10d5a
Hinson1Cheung <151332686+Hinson1Cheung@users.noreply.github.com>
2023-11-27 12:04:05 +0800

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,17 @@
+public static String dbAddress = "jdbc:mysql://projgw.cse.cuhk.edu.hk:2633/db60?autoReconnect=true&useSSL=false";
+public static String dbUsername = "Group60";
+public static String dbPassword = "CSCI3170";
 
+public static Connection connectToMySQL(){
+        Connection con = null;
+        try{
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                con = DriverManager.getConnection(dbAddress, dbUsername, dbPassword);
+        } catch (ClassNotFoundException e){
+                System.out.println("[Error]: Java MySQL DB Driver not found!!");
+                System.exit(0);
+        } catch (SQLException e){
+                System.out.println(e);
+        }
+        return con;
+}
