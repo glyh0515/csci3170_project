8fec4041ef3e0610c40817d79289cba49fbb0ddb
Hinson1Cheung <151332686+Hinson1Cheung@users.noreply.github.com>
2023-11-27 14:06:14 +0800

    Update main.java

--- main.java
+++ main.java
@@ -15,3 +15,12 @@ public static Connection connectToMySQL(){
         }
         return con;
 }
+
+public void CreateAll() throws SQLException{
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate("CREATE TABLE Category(CID INT(1) UNSIGNED NOT NULL, CNAME CHAR(20) NOT NULL, PRIMARY KEY(CID)");
+        stmt.executeUpdate("CREATE TABLE Manufacturer(MID INT(2) UNSIGNED NOT NULL, MNAME CHAR(20) NOT NULL, MADDRESS CHAR(50) NOT NULL, MPHONENUM INT(8) UNSIGNED NOT NULL, PRIMARY KEY (MID)");
+        stmt.executeUpdate("CREATE TABLE Salesperson(SID INT(2) UNSIGNED NOT NULL, SNAME CHAR(20) NOT NULL, SADDRESS CHAR(50) NOT NULL, SPHONENUM INT(8) NOT NULL, SEXPER INT(1) NOT NULL, PRIMARY KEY (SID)")
+        stmt.executeUpdate("CREATE TABLE Part(PID INT(3) UNSIGNED NOT NULL, PNAME CHAR(20) NOT NULL, PPRICE INT(5) UNSIGNED NOT NULL, ")
+        stmt.executeUpdate("")
+}
