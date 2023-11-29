d5c07cfcf978fa17c65f97a4e9160a21e58bbd89
Fai <hahafai1997@gmail.com>
2023-11-29 18:54:37 +0800

    Add basic message dialog to each model

--- Manager.java
+++ Manager.java
@@ -6,4 +6,29 @@ public class Manager extends BaseModel {
     public Manager(Connection connection, Scanner scanner) {
         super(connection, scanner);
     }
+
+    public void showManagerPanel() {
+        while (true) {
+
+            System.out.println("----Operations for adminstrator menu----");
+            System.out.println("What kinds of operation would you like to perform?");
+            System.out.println("1. List all salespersons");
+            System.out.println(
+                    "2. Count the no. of sales record of each salesperson under a specific range on years of experience");
+            System.out.println("3. Show the total sales value of each manufacturer");
+            System.out.println("4. Show the N most popular part");
+            System.out.println("5. Return to the main menu");
+            int choice = getValidChoice(1, 5);
+
+            switch (choice) {
+                case 1:
+
+                    break;
+                case 5:
+                    return;
+                default:
+                    break;
+            }
+        }
+    }
 }
--- SalesPerson.java
+++ SalesPerson.java
@@ -6,4 +6,29 @@ public class SalesPerson extends BaseModel {
     public SalesPerson(Connection connection, Scanner scanner) {
         super(connection, scanner);
     }
+
+    public void showSalesPersonPanel() {
+        while (true) {
+
+            System.out.println("----Operations for adminstrator menu----");
+            System.out.println("What kinds of operation would you like to perform?");
+            System.out.println("1. Search for parts");
+            System.out.println("2. Sell a part");
+            System.out.println("3. Return to the main menu");
+            int choice = getValidChoice(1, 3);
+
+            switch (choice) {
+                case 1:
+
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    return;
+
+                default:
+                    break;
+            }
+        }
+    }
 }
