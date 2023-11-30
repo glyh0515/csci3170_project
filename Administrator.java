8b7c0ae5fc2afe6952bb1f5d03390db06eddb720
Fai <hahafai1997@gmail.com>
2023-11-30 12:55:27 +0800

    updagte admin.java

--- Administrator.java
+++ Administrator.java
@@ -63,11 +63,16 @@ public class Administrator extends BaseModel {
 
 	private void createAll() throws SQLException {
 		Statement stmt = connection.createStatement();
-		stmt.executeUpdate("CREATE TABLE Category(CID INT(1) UNSIGNED NOT NULL, CNAME CHAR(20) NOT NULL, PRIMARY KEY(CID))");
-		stmt.executeUpdate("CREATE TABLE Manufacturer(MID INT(2) UNSIGNED NOT NULL, MNAME CHAR(20) NOT NULL, MADDRESS CHAR(50) NOT NULL, MPHONENUM INT(8) UNSIGNED NOT NULL, PRIMARY KEY (MID))");
-		stmt.executeUpdate("CREATE TABLE Part(PID INT(3) UNSIGNED NOT NULL, PNAME CHAR(20) NOT NULL, PPRICE INT(5) UNSIGNED NOT NULL, MID INT(2) UNSIGNED NOT NULL, CID INT(1) UNSIGNED NOT NULL, PWAR INT(2) UNSIGNED NOT NULL, PQTY INT(2) UNSIGNED NOT NULL, PRIMARY KEY (PID), FOREIGN KEY (MID) REFERENCES Manufacturer(MID), FOREIGN KEY (CID) REFERENCES Category(CID))");
-		stmt.executeUpdate("CREATE TABLE Salesperson(SID INT(2) UNSIGNED NOT NULL, SNAME CHAR(20) NOT NULL, SADDRESS CHAR(50) NOT NULL, SPHONENUM INT(8) UNSIGNED NOT NULL, SEXPER INT(1) UNSIGNED NOT NULL, PRIMARY KEY (SID))");
-		stmt.executeUpdate("CREATE TABLE Transaction(TID INT(4) UNSIGNED NOT NULL, PID INT(3) UNSIGNED NOT NULL, SID INT(2) UNSIGNED NOT NULL, TDATE DATE, PRIMARY KEY (TID), FOREIGN KEY (PID) REFERENCES Part(PID), FOREIGN KEY (SID) REFERENCES Salesperson(SID))");
+		stmt.executeUpdate(
+				"CREATE TABLE Category(CID INT(1) UNSIGNED NOT NULL, CNAME CHAR(20) NOT NULL, PRIMARY KEY(CID))");
+		stmt.executeUpdate(
+				"CREATE TABLE Manufacturer(MID INT(2) UNSIGNED NOT NULL, MNAME CHAR(20) NOT NULL, MADDRESS CHAR(50) NOT NULL, MPHONENUM INT(8) UNSIGNED NOT NULL, PRIMARY KEY (MID))");
+		stmt.executeUpdate(
+				"CREATE TABLE Part(PID INT(3) UNSIGNED NOT NULL, PNAME CHAR(20) NOT NULL, PPRICE INT(5) UNSIGNED NOT NULL, MID INT(2) UNSIGNED NOT NULL, CID INT(1) UNSIGNED NOT NULL, PWAR INT(2) UNSIGNED NOT NULL, PQTY INT(2) UNSIGNED NOT NULL, PRIMARY KEY (PID), FOREIGN KEY (MID) REFERENCES Manufacturer(MID), FOREIGN KEY (CID) REFERENCES Category(CID))");
+		stmt.executeUpdate(
+				"CREATE TABLE Salesperson(SID INT(2) UNSIGNED NOT NULL, SNAME CHAR(20) NOT NULL, SADDRESS CHAR(50) NOT NULL, SPHONENUM INT(8) UNSIGNED NOT NULL, SEXPER INT(1) UNSIGNED NOT NULL, PRIMARY KEY (SID))");
+		stmt.executeUpdate(
+				"CREATE TABLE Transaction(TID INT(4) UNSIGNED NOT NULL, PID INT(3) UNSIGNED NOT NULL, SID INT(2) UNSIGNED NOT NULL, TDATE DATE, PRIMARY KEY (TID), FOREIGN KEY (PID) REFERENCES Part(PID), FOREIGN KEY (SID) REFERENCES Salesperson(SID))");
 	}
 
 	private void deleteAll() throws SQLException {
@@ -79,14 +84,14 @@ public class Administrator extends BaseModel {
 		stmt.executeUpdate("DROP TABLE Transaction");
 	}
 
-	private void load(Connection con) throws SQLException{
-        Statement stmt = con.createStatement();
-        stmt.execute("LOAD DATA INFILE 'category.txt' REPLACE INTO TABLE Category");
+	private void load() throws SQLException {
+		Statement stmt = connection.createStatement();
+		stmt.execute("LOAD DATA INFILE 'category.txt' REPLACE INTO TABLE Category");
 		stmt.execute("LOAD DATA INFILE 'manufacturer.txt' REPLACE INTO TABLE Manufacturer");
 		stmt.execute("LOAD DATA INFILE 'part.txt' REPLACE INTO TABLE Part");
 		stmt.execute("LOAD DATA INFILE 'salesperson.txt' REPLACE INTO TABLE Salesperson");
 
-    }
+	}
 
 	private void showContentOfTable() throws SQLException {
 		System.out.print("Which table would you like to show: ");
