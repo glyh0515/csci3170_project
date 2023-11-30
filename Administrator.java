725cb3429f1d1094a3fa81eefff661e310aae1ef
Fai <hahafai1997@gmail.com>
2023-11-30 13:40:06 +0800

    Change table names to lowercases

--- Administrator.java
+++ Administrator.java
@@ -65,41 +65,40 @@ public class Administrator extends BaseModel {
 		Statement stmt = connection.createStatement();
 		// Category
 		stmt.executeUpdate(
-				"CREATE TABLE Category(CID INT(1) UNSIGNED NOT NULL, CNAME CHAR(20) NOT NULL, PRIMARY KEY(CID))");
+				"CREATE TABLE category(CID INT(1) UNSIGNED NOT NULL, CNAME CHAR(20) NOT NULL, PRIMARY KEY(CID))");
 
 		// Manufacturer
 		stmt.executeUpdate(
-				"CREATE TABLE Manufacturer(MID INT(2) UNSIGNED NOT NULL, MNAME CHAR(20) NOT NULL, MADDRESS CHAR(50) NOT NULL, MPHONENUM INT(8) UNSIGNED NOT NULL, PRIMARY KEY (MID))");
+				"CREATE TABLE manufacturer(MID INT(2) UNSIGNED NOT NULL, MNAME CHAR(20) NOT NULL, MADDRESS CHAR(50) NOT NULL, MPHONENUM INT(8) UNSIGNED NOT NULL, PRIMARY KEY (MID))");
 
 		// Part
 		stmt.executeUpdate(
-				"CREATE TABLE Part(PID INT(3) UNSIGNED NOT NULL, PNAME CHAR(20) NOT NULL, PPRICE INT(5) UNSIGNED NOT NULL, MID INT(2) UNSIGNED NOT NULL, CID INT(1) UNSIGNED NOT NULL, PWAR INT(2) UNSIGNED NOT NULL, PQTY INT(2) UNSIGNED NOT NULL, PRIMARY KEY (PID), FOREIGN KEY (MID) REFERENCES Manufacturer(MID), FOREIGN KEY (CID) REFERENCES Category(CID))");
+				"CREATE TABLE part(PID INT(3) UNSIGNED NOT NULL, PNAME CHAR(20) NOT NULL, PPRICE INT(5) UNSIGNED NOT NULL, MID INT(2) UNSIGNED NOT NULL, CID INT(1) UNSIGNED NOT NULL, PWAR INT(2) UNSIGNED NOT NULL, PQTY INT(2) UNSIGNED NOT NULL, PRIMARY KEY (PID), FOREIGN KEY (MID) REFERENCES manufacturer(MID), FOREIGN KEY (CID) REFERENCES category(CID))");
 
 		// Salesperson
 		stmt.executeUpdate(
-				"CREATE TABLE Salesperson(SID INT(2) UNSIGNED NOT NULL, SNAME CHAR(20) NOT NULL, SADDRESS CHAR(50) NOT NULL, SPHONENUM INT(8) UNSIGNED NOT NULL, SEXPER INT(1) UNSIGNED NOT NULL, PRIMARY KEY (SID))");
+				"CREATE TABLE salesperson(SID INT(2) UNSIGNED NOT NULL, SNAME CHAR(20) NOT NULL, SADDRESS CHAR(50) NOT NULL, SPHONENUM INT(8) UNSIGNED NOT NULL, SEXPER INT(1) UNSIGNED NOT NULL, PRIMARY KEY (SID))");
 
 		// Transaction
 		stmt.executeUpdate(
-				"CREATE TABLE Transaction(TID INT(4) UNSIGNED NOT NULL, PID INT(3) UNSIGNED NOT NULL, SID INT(2) UNSIGNED NOT NULL, TDATE DATE, PRIMARY KEY (TID), FOREIGN KEY (PID) REFERENCES Part(PID), FOREIGN KEY (SID) REFERENCES Salesperson(SID))");
+				"CREATE TABLE transaction(TID INT(4) UNSIGNED NOT NULL, PID INT(3) UNSIGNED NOT NULL, SID INT(2) UNSIGNED NOT NULL, TDATE DATE, PRIMARY KEY (TID), FOREIGN KEY (PID) REFERENCES part(PID), FOREIGN KEY (SID) REFERENCES salesperson(SID))");
 	}
 
 	private void deleteAll() throws SQLException {
 		Statement stmt = connection.createStatement();
-		stmt.executeUpdate("DROP TABLE Transaction");
-		stmt.executeUpdate("DROP TABLE Part");
-		stmt.executeUpdate("DROP TABLE Category");
-		stmt.executeUpdate("DROP TABLE Manufacturer");
-		stmt.executeUpdate("DROP TABLE Salesperson");
-
+		stmt.executeUpdate("DROP TABLE transaction");
+		stmt.executeUpdate("DROP TABLE part");
+		stmt.executeUpdate("DROP TABLE category");
+		stmt.executeUpdate("DROP TABLE manufacturer");
+		stmt.executeUpdate("DROP TABLE salesperson");
 	}
 
 	private void load() throws SQLException {
 		Statement stmt = connection.createStatement();
-		stmt.execute("LOAD DATA INFILE 'category.txt' REPLACE INTO TABLE Category");
-		stmt.execute("LOAD DATA INFILE 'manufacturer.txt' REPLACE INTO TABLE Manufacturer");
-		stmt.execute("LOAD DATA INFILE 'part.txt' REPLACE INTO TABLE Part");
-		stmt.execute("LOAD DATA INFILE 'salesperson.txt' REPLACE INTO TABLE Salesperson");
+		stmt.execute("LOAD DATA INFILE 'category.txt' REPLACE INTO TABLE category");
+		stmt.execute("LOAD DATA INFILE 'manufacturer.txt' REPLACE INTO TABLE manufacturer");
+		stmt.execute("LOAD DATA INFILE 'part.txt' REPLACE INTO TABLE part");
+		stmt.execute("LOAD DATA INFILE 'salesperson.txt' REPLACE INTO TABLE salesperson");
 
 	}
 
@@ -108,6 +107,9 @@ public class Administrator extends BaseModel {
 		String tableName = scanner.nextLine();
 		System.out.println("Content of table " + tableName);
 		Statement stmt = connection.createStatement();
-		stmt.executeQuery(String.format("SELECT * FROM %s", tableName));
+		ResultSet rs = stmt.executeQuery(String.format("SELECT * FROM %s", tableName));
+		while (rs.next()) {
+
+		}
 	}
 }
