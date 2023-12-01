72575928cd04cb5c211a76a477c186f4308626f8
Gordon Lee <1155176301@link.cuhk.edu.hk>
2023-12-01 12:39:46 +0800

    Update manager function

--- Manager.java
+++ Manager.java
@@ -37,6 +37,11 @@ public class Manager extends BaseModel {
                     }
                     break;
                 case 3:
+                    try {
+                        ListManufacturers();
+                    } catch (SQLException e) {
+                        System.out.println(e);
+                    }
                     break;
                 case 4:
                     try {
@@ -63,7 +68,7 @@ public class Manager extends BaseModel {
         Statement stmt = connection.createStatement();
         ResultSet rs = stmt.executeQuery(
                 String.format("SELECT sID, sName, sPhoneNum, sExperience FROM salesperson ORDER BY sExperience %s",
-                        (order == 1 ? "ASC" : "DESC")));
+                        (order == 1 ? "ASC;" : "DESC;")));
         while (rs.next()) {
             System.out.println(String.format("| %d | %s | %d | %d |", rs.getInt("sID"), rs.getString("sName"),
                     rs.getInt("sPhoneNum"), rs.getInt("sExperience")));
@@ -78,7 +83,7 @@ public class Manager extends BaseModel {
         System.out.println("| ID | Name | Years of Experience | Number of Transaction |");
         Statement stmt = connection.createStatement();
         ResultSet rs = stmt.executeQuery(String.format(
-            "SELECT S.sID, S.sName, S.sExperience, T.SALE_TRANSACTION FROM (SELECT sID, sName, sExperience from salesperson) S, (SELECT sID, count(*) AS SALE_TRANSACTION from transaction GROUP by sID) T WHERE S.sID = T.sID AND S.sExperience >= %d AND S.sExperience <= %d GROUP BY S.sID, S.sName, S.sExperience ORDER BY S.sID DESC"     
+            "SELECT S.sID, S.sName, S.sExperience, T.SALE_TRANSACTION FROM (SELECT sID, sName, sExperience from salesperson) S, (SELECT sID, count(*) AS SALE_TRANSACTION from transaction GROUP by sID) T WHERE S.sID = T.sID AND S.sExperience >= %d AND S.sExperience <= %d GROUP BY S.sID, S.sName, S.sExperience ORDER BY S.sID DESC;"     
         , lowerBound, upperBound));
         while (rs.next()) {
             System.out.println(String.format("| %d | %s | %d | %d", rs.getInt("S.sID"), rs.getString("S.sName"),
@@ -88,6 +93,21 @@ public class Manager extends BaseModel {
     }
 
     public void ListManufacturers() throws SQLException {
+        System.out.println("| Manufacturer ID | Manufacturer Name | Total Sales Value |");
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery(String.format(
+                "SELECT M.mID, M.mName, SUM(T.PART_SALE_COUNT * P.pPrice) AS TOTAL_SALES_VALUE\r\n" + //
+                        "FROM (SELECT mID, mName from manufacturer) M, \r\n" + //
+                        "(SELECT pID, pPrice, mID from part) P,\r\n" + //
+                        "(SELECT pID, COUNT(*) AS PART_SALE_COUNT from transaction GROUP BY pID) T\r\n" + //
+                        "WHERE M.mID = P.mID AND P.pID = T.pID\r\n" + //
+                        "GROUP BY M.mID, M.mName\r\n" + //
+                        "ORDER BY TOTAL_SALES_VALUE DESC;\r\n" + //
+                        ""));
+        while (rs.next()) {
+            System.out.println(String.format("| %d | %s | %d |", rs.getInt("M.mID"), rs.getString("M.mName"),
+                    rs.getInt("TOTAL_SALES_VALUE")));
+        }
         return;
     }
 
@@ -97,7 +117,7 @@ public class Manager extends BaseModel {
         System.out.println("| Part ID | Part Name | No. of Transaction |");
         Statement stmt = connection.createStatement();
         ResultSet rs = stmt.executeQuery(String.format(
-                "SELECT P.pID, P.pName, SUM(T.PART_TRANSACTION) AS TOTAL_TRANSACTION FROM (SELECT pID, pName FROM part) P, (SELECT pID, COUNT(*) AS PART_TRANSACTION FROM transaction GROUP BY pID) T WHERE P.pID = T.pID GROUP BY P.pID, pName HAVING SUM(T.PART_TRANSACTION) > 0 ORDER BY TOTAL_TRANSACTION DESC LIMIT %d",
+                "SELECT P.pID, P.pName, SUM(T.PART_TRANSACTION) AS TOTAL_TRANSACTION FROM (SELECT pID, pName FROM part) P, (SELECT pID, COUNT(*) AS PART_TRANSACTION FROM transaction GROUP BY pID) T WHERE P.pID = T.pID GROUP BY P.pID, pName HAVING SUM(T.PART_TRANSACTION) > 0 ORDER BY TOTAL_TRANSACTION DESC LIMIT %d;",
                 N));
         while (rs.next()) {
             System.out.println(String.format("| %d | %s | %d |", rs.getInt("P.pId"), rs.getString("P.pName"),
