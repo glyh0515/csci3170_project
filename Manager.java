b02ce45d92137aabc14b3912a1660fea64b4721d
Gordon Lee <1155176301@link.cuhk.edu.hk>
2023-12-01 11:24:53 +0800

    Update manager function

--- Manager.java
+++ Manager.java
@@ -23,17 +23,22 @@ public class Manager extends BaseModel {
 
             switch (choice) {
                 case 1:
+                    try {
+                        ListSalespersons();
+                    } catch (SQLException e) {
+                        System.out.println(e);
+                    }
                     break;
                 case 2:
                     break;
                 case 3:
                     break;
                 case 4:
-                try {
-                    ShowNParts();
-                } catch (SQLException e) {
-                    System.out.println(e);
-                }
+                    try {
+                        ShowNParts();
+                    } catch (SQLException e) {
+                        System.out.println(e);
+                    }
                     break;
                 case 5:
                     return;
@@ -48,18 +53,32 @@ public class Manager extends BaseModel {
         System.out.println("1. By ascending order");
         System.out.println("2. By descending order");
         System.out.print("Choose the list ordering: ");
-        /* "SELECT sID, sPhone, sName, sExperience FROM SALESPERSON ORDER BY sExperience"
-         * + (order == 1 ? "ASC" : "DESC") + ";"
-        */
+        int order = scanner.nextInt();
+        System.out.println("| ID | Name | Mobile Phone | Year of Experience |");
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery(
+                String.format("SELECT sID, sName, sPhoneNum, sExperience FROM salesperson ORDER BY sExperience %s",
+                        (order == 1 ? "ASC" : "DESC")));
+        while (rs.next()) {
+            System.out.println(String.format("| %d | %s | %d | %d |", rs.getInt("sID"), rs.getString("sName"),
+                    rs.getInt("sPhoneNum"), rs.getInt("sExperience")));
+        }
     }
+
+    /*
+     * "SELECT sID, sPhone, sName, sExperience FROM SALESPERSON ORDER BY sExperience"
+     * + (order == 1 ? "ASC" : "DESC") + ";"
+     */
 
     public void CountTransaction() throws SQLException {
         System.out.print("Type in the lower bound for years of experience: ");
         System.out.print("Type in the upper bound for years of experience: ");
-        /* "SELECT S.sID, S.sName, S.sExperience, COUNT(T.sID) 
-        FROM SALESPERSON S, TRANSATION T
-        WHERE S.sExpeirience >= " + lowerBound + " AND S.sExperience <= " + upperBound +
-        "GROUP BY T.sID ORDER BY s.SID DESC"
+        /*
+         * "SELECT S.sID, S.sName, S.sExperience, COUNT(T.sID)
+         * FROM SALESPERSON S, TRANSATION T
+         * WHERE S.sExpeirience >= " + lowerBound + " AND S.sExperience <= " +
+         * upperBound +
+         * "GROUP BY T.sID ORDER BY s.SID DESC"
          * 
          */
         return;
@@ -74,9 +93,12 @@ public class Manager extends BaseModel {
         int N = scanner.nextInt();
         System.out.println("| Part ID | Part Name | No. of Transaction |");
         Statement stmt = connection.createStatement();
-		ResultSet rs = stmt.executeQuery(String.format("SELECT P.pID, P.pName, SUM(T.PART_TRANSACTION) AS TOTAL_TRANSACTION FROM (SELECT pID, pName FROM part) P, (SELECT pID, COUNT(*) AS PART_TRANSACTION FROM transaction GROUP BY pID) T WHERE P.pID = T.pID GROUP BY P.pID, pName HAVING SUM(T.PART_TRANSACTION) > 0 ORDER BY TOTAL_TRANSACTION DESC LIMIT %d", N));
+        ResultSet rs = stmt.executeQuery(String.format(
+                "SELECT P.pID, P.pName, SUM(T.PART_TRANSACTION) AS TOTAL_TRANSACTION FROM (SELECT pID, pName FROM part) P, (SELECT pID, COUNT(*) AS PART_TRANSACTION FROM transaction GROUP BY pID) T WHERE P.pID = T.pID GROUP BY P.pID, pName HAVING SUM(T.PART_TRANSACTION) > 0 ORDER BY TOTAL_TRANSACTION DESC LIMIT %d",
+                N));
         while (rs.next()) {
-            System.out.println(String.format("| %d | %s | %d |", rs.getInt("P.pId"), rs.getString("P.pName"), rs.getInt("TOTAL_TRANSACTION")));
+            System.out.println(String.format("| %d | %s | %d |", rs.getInt("P.pId"), rs.getString("P.pName"),
+                    rs.getInt("TOTAL_TRANSACTION")));
         }
         return;
     }
